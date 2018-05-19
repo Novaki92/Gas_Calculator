7a07340acbd86ba17f10e9dc24e49a3a447d3c7d
Nathan "Novaki" Ethridge <nathane2010@yahoo.com>
2018-05-19 16:23:56 -0500

    Readability
    Updated readability and conversion method to prevent negatives

--- Gas_Calculator/Gas_Calculator/Form1.cs
+++ Gas_Calculator/Gas_Calculator/Form1.cs
@@ -19,42 +19,47 @@ namespace Gas_Calculator
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int mpg = 1;
+            int mpg = 1; //prevents divide by zero
             int miles = 0;
             decimal gas = 0;
 
-            try
-            {
-                mpg = Convert.ToInt16(tb_MPG.Text);
-                miles = Convert.ToInt16(tb_Miles.Text);
-                gas = Convert.ToDecimal(tb_Gas.Text);
+            //conversion
+            try{
+                mpg = Math.Abs(Convert.ToInt16(tb_MPG.Text));
+                miles = Math.Abs(Convert.ToInt16(tb_Miles.Text));
+                gas = Math.Abs(Convert.ToDecimal(tb_Gas.Text));
             }
-            catch
-            {
-                MessageBox.Show("Enter a valid number");
+            catch{
+                //MessageBox.Show("Enter a valid number");
             }
 
+            //math
             decimal price = (miles / mpg) * gas;
-            if (cb_trip.Checked)
-            {
-                price *= 2;
+            if (cb_trip.Checked){price *= 2;}
+
+            //label
+            lbl_result.Text = "$" + Convert.ToString(price);
+            if (mpg == 1 || miles == 0 || gas == 0){
+                lbl_result.Visible = false;
+            } else{
+                lbl_result.Visible = true;
             }
 
-            lbl_result.Text = "$" + Convert.ToString(price);
-
-            if (mpg == 1 || miles == 0 || gas == 0)
-            {
-                lbl_result.Visible = false;
-            } else
-            {
-                lbl_result.Visible = true;
+            //remove negative from textbox
+            if (tb_MPG.Text.Contains('-')){
+                tb_MPG.Text = tb_MPG.Text.Remove(0, 1);
+            }
+            if (tb_Miles.Text.Contains('-')){
+                tb_Miles.Text = tb_Miles.Text.Remove(0, 1);
+            }
+            if(tb_Gas.Text.Contains('-')){
+                tb_Gas.Text = tb_Gas.Text.Remove(0, 1);
             }
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Return)
-            {
+            if (e.KeyCode == Keys.Return){
                 button1_Click(sender, e);
             }
         }
